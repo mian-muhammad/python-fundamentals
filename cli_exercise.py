2dec7e73838c1cf8b9924ac9e4ca9072b6458983
Mian Muhammad <mianmuhammad80@gmail.com>
2022-10-27 13:15:42 +0500

    CLI

---
+++ cli_exercise.py
@@ -0,0 +1,11 @@
+# import sys
+
+# args = sys.argv[1:]
+
+# print(args)
+
+name = input("Hello, what is your name? ")
+
+birthday_string = input(f"Hello {name}. Please enter your birthday in MM/DD/YYYY format: ")
+
+print(f"Hello {name}. Your birthday is on {birthday_string}.")
